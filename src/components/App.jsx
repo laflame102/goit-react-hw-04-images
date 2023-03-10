b2964e1946757af57c0b54fb5a39c9b3878a3acd
laflame102 <laflame4308@gmail.com>
2023-03-10 13:18:53 +0200

    moved logic from ImageGallery to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,51 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { pixabay } from 'services';
+import { Loader } from 'components/Loader/Loader';
 
 export const App = () => {
   const [input, setInput] = useState('');
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await pixabay(input, page);
+        setImages(prevImages => [...prevImages, ...response.hits]);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchImages();
+  }, [input, page]);
 
   const handleFormSubmit = inputValue => {
     setInput(inputValue);
+    setPage(1);
+    setLoading(true);
+    setImages([]);
+  };
+
+  const handleLoad = () => {
+    setLoading(true);
+    setPage(prevPage => prevPage + 1);
   };
 
   return (
     <div>
       <Searchbar onSubmit={handleFormSubmit} />
-      <ImageGallery inputValue={input} />
+      {loading && <Loader />}
+      <ImageGallery items={images} inputValue={input} />
+      {images.length > 0 && <Button onLoad={handleLoad} />}
       <ToastContainer />
     </div>
   );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,35 +1,35 @@
-import { toast } from 'react-toastify';
-import { pixabay } from 'services';
-import { useState, useEffect } from 'react';
+// import { toast } from 'react-toastify';
+// import { pixabay } from 'services';
+import { useState } from 'react';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { Button } from 'components/Button/Button';
-import { Loader } from 'components/Loader/Loader';
+// import { Button } from 'components/Button/Button';
+// import { Loader } from 'components/Loader/Loader';
 import { Modal } from 'components/Modal/Modal';
 
-export const ImageGallery = ({ inputValue }) => {
-  const [images, setImages] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState(1);
+export const ImageGallery = ({ items, inputValue }) => {
+  // const [images, setImages] = useState([]);
+  // const [loading, setLoading] = useState(false);
+  // const [page, setPage] = useState(1);
   const [showModal, setShowModal] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
 
-  useEffect(() => {
-    const fetchImages = async () => {
-      setLoading(true);
-      setPage(1);
+  // useEffect(() => {
+  //   const fetchImages = async () => {
+  //     // setLoading(true);
+  //     // setPage(1);
 
-      try {
-        const response = await pixabay(inputValue, page);
-        setImages(prevImages => [...prevImages, ...response.hits]);
-      } catch (error) {
-        toast.error(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
+  //     try {
+  //       const response = await pixabay(inputValue, page);
+  //       setImages(prevImages => [...prevImages, ...response.hits]);
+  //     } catch (error) {
+  //       toast.error(error.message);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    fetchImages();
-  }, [inputValue, page]);
+  //   fetchImages();
+  // }, [inputValue, page]);
 
   // async componentDidUpdate(prevProps, prevState) {
   //   if (
@@ -53,10 +53,10 @@ export const ImageGallery = ({ inputValue }) => {
   //   }
   // }
 
-  const handleLoad = () => {
-    setLoading(true);
-    setPage(prevPage => prevPage + 1);
-  };
+  // const handleLoad = () => {
+  //   setLoading(true);
+  //   setPage(prevPage => prevPage + 1);
+  // };
 
   const toggleModal = image => {
     //  this.setState(({ showModal }) => ({ showModal: !showModal }));
@@ -66,10 +66,10 @@ export const ImageGallery = ({ inputValue }) => {
 
   return (
     <div>
-      {loading && <Loader />}
-      {images && (
+      {/* {loading && <Loader />} */}
+      {items && (
         <ul className="ImageGallery">
-          {images.map(({ id, webformatURL, largeImageURL }) => (
+          {items.map(({ id, webformatURL, largeImageURL }) => (
             <li
               key={id}
               className="ImageGalleryItem"
@@ -85,7 +85,7 @@ export const ImageGallery = ({ inputValue }) => {
           <img src={selectedImage} alt="g" />
         </Modal>
       )}
-      {images.length > 0 && <Button onLoad={handleLoad} />}
+      {/* {images.length > 0 && <Button onLoad={handleLoad} />} */}
     </div>
   );
 };
